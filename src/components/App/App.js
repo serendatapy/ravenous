0c587bd77a2111e902688928eb79b0a128a6a9d7
serendatapy <arvaldivia@gmail.com>
2020-08-05 14:50:14 +0200

    finished frontend of ravenous project

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,16 +1,15 @@
 import React from 'react';
 import './App.css';
 import BusinessList from '../BusinessList/BusinessList';
-import Business from '../Business/Business'
+import Business from '../Business/Business';
+import SearchBar from '../SearchBar/SearchBar'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+        <h1>ravenous</h1>
+        <SearchBar/>
         <BusinessList />
-        {/* <Business /> */}
-
-      </header>
     </div>
   );
 }
