32e878c01f0aa1cc502063b17822edd19b42979c
serendatapy <arvaldivia@gmail.com>
2020-08-05 11:39:53 +0200

    Added BusinessList component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
+import BusinessList from '../BusinessList/BusinessList';
 import Business from '../Business/Business'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Business />
+        <BusinessList />
+        {/* <Business /> */}
 
       </header>
     </div>
